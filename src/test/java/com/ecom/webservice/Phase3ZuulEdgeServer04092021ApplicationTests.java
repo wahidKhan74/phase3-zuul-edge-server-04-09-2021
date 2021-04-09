e276fb0dd6a523ba2a085fee3a05b2d8abcd921c
Waheed Khan <wahid.khan74@gmail.com>
2021-04-09 06:25:00 +0000

    zuul proxy server setup

---
+++ src/test/java/com/ecom/webservice/Phase3ZuulEdgeServer04092021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3ZuulEdgeServer04092021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
